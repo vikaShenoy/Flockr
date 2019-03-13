6532c5a72dc2e1f58c0660f63ce62907df11249f
Andy Holden <ash102@uclive.ac.nz>
2019-03-13 21:59:55 +1300

    Made the destination card container file and began implementation.

---
+++ frontend/src/containers/Destinations/DestinationCard/DestinationCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card class="col-md-12 offset-md-0 d-flex">
+        Hello
+        <v-card class="col-md-3 offset-md-0 d-inline-block">
+            <h2 class="name-header">{{ dest.name }}</h2>
+        </v-card>
+        <v-card class="col-md-12">
+            <v-img src="src/assets/logo.png"></v-img>
+        </v-card>
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: "DestinationCard"
+  }
+</script>
+
+<style scoped>
+
+</style>
