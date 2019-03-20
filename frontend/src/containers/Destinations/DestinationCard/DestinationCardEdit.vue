a61b3c455e7e50ed41e8379e198790433a5da7ba
Andy Holden <ash102@uclive.ac.nz>
2019-03-20 15:00:12 +1300

    Finished implementation of the Destination Edit card and made a few tweaks to the style of the destinations page.

--- frontend/src/containers/Destinations/DestinationCard/DestinationCardEdit.vue
+++ frontend/src/containers/Destinations/DestinationCard/DestinationCardEdit.vue
@@ -2,41 +2,63 @@
     <v-card
             class="destination-card row col-md-12">
         <div class="title-card col-md-3">
-            <v-flex xs12 sm6 md3>
+            <div xs12 sm6 md3 class="name-header">
                 <v-text-field
-                        label="Name">
+                    label="Destination Name"
+                    :value="destination.name">
                 </v-text-field>
-            </v-flex>
+            </div>
             <div class="body-card col-md-12">
                 <v-img src="https://picsum.photos/510/300?random"></v-img>
             </div>
         </div>
         <div class="col-md-5">
             <div class="row">
-                <div class="basic-info"><p><b>Type</b></p></div>
-                <v-flex xs12 sm6 md3 class="basic-info">
+                <div class="basic-info-label"><p><b>Type</b></p></div>
+                <div xs12 sm6 md3 class="basic-info">
                     <v-text-field
                             label="Solo"
-                            placeholder="Destination Type"
+                            :value="destination.destType"
                             solo
                     ></v-text-field>
-                </v-flex>
+                </div>
             </div>
             <hr class="divider"/>
             <div class="row">
-                <div class="basic-info"><p><b>District</b></p></div><div class="basic-info">{{ destination.district }}</div>
+                <div class="basic-info-label"><p><b>District</b></p></div>
+                <div xs12 sm6 md3 class="basic-info">
+                    <v-text-field
+                            label="Solo"
+                            :value="destination.district"
+                            solo
+                    ></v-text-field>
+                </div>
             </div>
             <hr class="divider"/>
         </div>
         <div class="col-md-4">
-            <h2 class="name-header">{{ destination.country }}</h2>
-            <v-img src="https://cdn.mapsinternational.co.uk/pub/media/catalog/product/cache/afad95d7734d2fa6d0a8ba78597182b7/w/o/world-wall-map-political-without-flags_wm00001_h.jpg"></v-img>
+            <div xs12 sm6 md3 class="name-header">
+                <v-text-field
+                        label="Destination Country"
+                        :value="destination.country">
+                </v-text-field>
+            </div>
+            <div xs12 sm6 md3 class="name-header">
+                <v-text-field
+                        label="Destination Latitude"
+                        :value="destination.lon">
+                </v-text-field>
+            </div>
+            <div xs12 sm6 md3 class="name-header">
+                <v-text-field
+                        label="Destination Longitude"
+                        :value="destination.lat">
+                </v-text-field>
+            </div>
+            <!--<v-img class="map" src="https://cdn.mapsinternational.co.uk/pub/media/catalog/product/cache/afad95d7734d2fa6d0a8ba78597182b7/w/o/world-wall-map-political-without-flags_wm00001_h.jpg"></v-img>-->
         </div>
-        <v-btn fab dark class="edit-button" id="edit-destination-button">
-            <v-icon dark>edit</v-icon>
-        </v-btn>
-        <v-btn fab dark class="delete-button" id="delete-destination-button">
-            <v-icon dark>remove</v-icon>
+        <v-btn fab dark class="edit-button" id="save-destination-button">
+            <v-icon dark>check_circle</v-icon>
         </v-btn>
     </v-card>
 </template>
@@ -65,6 +87,14 @@
         country: {
           type: String,
           required: true
+        },
+        lat: {
+          type: String,
+          required: true
+        },
+        lon: {
+          type: String,
+          required: true
         }
       }
     }
@@ -78,7 +108,17 @@
         margin: 0 0 20px;
     }
 
+    .map {
+        margin: 0 0 20px;
+    }
+
     .basic-info {
+        text-align: center;
+        width: 50%;
+    }
+
+    .basic-info-label {
+        margin: 10px 0 0;
         text-align: center;
         width: 50%;
     }
@@ -90,28 +130,16 @@
 
     .destination-card {
         width: 100%;
-        display: flex;
+        margin: 10px 0 0;
     }
 
-    .destinations-panel :hover #delete-destination-button {
+    .destinations-panel :hover #save-destination-button {
         visibility: visible;
     }
 
-    .destinations-panel :hover #edit-destination-button {
-        visibility: visible;
-    }
-
-    .destination-card #edit-destination-button {
+    .destination-card #save-destination-button {
         position: absolute;
         top: 30px;
-        right: 30px;
-        visibility: hidden;
-
-    }
-
-    .destination-card #delete-destination-button {
-        position: absolute;
-        bottom: 30px;
         right: 30px;
         visibility: hidden;
     }
