d515e91ea873c925e6a9676227226f05d93648ba
vsh33 <vsh33@uclive.ac.nz>
2019-03-25 11:03:44 +1300

    Implemented delete trip endpoint. - Can now delete a trip by passing user and trip IDs. - Waiting on post endpoint to be completed before it can be tested. #story[207]

--- backend/app/repository/TripRepository.java
+++ backend/app/repository/TripRepository.java
@@ -3,6 +3,7 @@ package repository;
 import models.Trip;
 
 import javax.inject.Inject;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -26,6 +27,34 @@ public class TripRepository {
     public CompletionStage<Trip> saveTrip(Trip trip) {
         return supplyAsync(() -> {
             trip.save();
+            return trip;
+        }, executionContext);
+    }
+
+    /**
+     * Updates a trip in the database.
+     * @param trip The trip to update changes.
+     * @return The trip that was updated.
+     */
+    public CompletionStage<Trip> update(Trip trip) {
+        return supplyAsync(() -> {
+            trip.update();
+            return trip;
+        }, executionContext);
+    }
+
+    /**
+     * Get a trip by its tripId and userId.
+     * @param tripId The id of the trip to find.
+     * @param userId The user id of the trip to find.
+     * @return the trip that matches given ids.
+     */
+    public CompletionStage<Optional<Trip>> getTripByIds(int tripId, int userId) {
+        return supplyAsync(() -> {
+            Optional<Trip> trip = Trip.find.query().
+                    where().eq("trip_id", tripId)
+                    .eq("user_id", userId)
+                    .findOneOrEmpty();
             return trip;
         }, executionContext);
     }
