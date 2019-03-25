73f3fde65b95a299c52c3ae15ef4b6e607360cfe
Exequiel Bahamonde Cárcamo <eba54@uclive.ac.nz>
2019-03-25 23:29:49 +1300

    Create static fields for trips

---
+++ frontend/src/containers/Trips/TimelineDestination.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-timeline-item :style="{width: '90%', marginLeft: '5%'}" color="primary" small :right="alignRight">
+            <v-card>
+                <v-card-title class="primary">
+                    <h3 class="white--text font-weight-light">{{destination.destinationName}}</h3>
+                </v-card-title>
+                <v-container>
+                    <v-layout>
+                        <v-flex xs2>
+                            <v-icon size="30">flight_takeoff</v-icon>
+                        </v-flex>
+                        <v-flex xs10>
+                            <p>{{destination.arrivalDate}}</p>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout>
+                        <v-flex xs2>
+                            <v-icon size="30">flight_landing</v-icon>
+                        </v-flex>
+                        <v-flex xs10>
+                            <p>{{destination.departureDate}}</p>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-card>
+        </v-timeline-item>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'TimelineDestination',
+    props: {
+        destination: {
+            destinationName: String,
+            arrivalDate: String,
+            departureDate: String,
+        },
+        alignRight: Boolean
+    }
+}
+</script>
---
+++ frontend/src/containers/Trips/Trip.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="trip-container">
+        <v-card>
+            <h2>{{trip.tripName}}</h2>
+            <v-timeline>
+                <TimelineDestination
+                    v-for="(destination, index) in trip.destinations"
+                    :key="destination.destinationName"
+                    :destination="destination"
+                    :alignRight="index % 2 === 0 ? true : false"
+                />
+            </v-timeline>
+        </v-card>
+    </div>    
+</template>
+
+
+<script>
+import TimelineDestination from './TimelineDestination';
+export default {
+    name: 'Trip',
+    props: {
+        trip: {
+            tripName: String,
+            destinations: Object
+        }
+    },
+    components: {
+        TimelineDestination
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .trip-container {
+        width: 100%;
+    }
+
+    h2 {
+        text-align: center;
+        padding: 15px;
+    }
+</style>
