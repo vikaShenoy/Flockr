4fcd7e3773748f1940636abd9ca901396792b813
Rafael Goesmann <rgo51@uclive.ac.nz>
2019-04-16 15:02:33 +1200

    Added backend URL retrieved from environment variable

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,19 @@
+const env = process.env.NODE_ENV;
+
+let backendUrl = "";
+
+switch (env) {
+  case "production":
+    backendUrl = "http://localhost:443";
+    break;
+  case "staging":
+    backendUrl = "http://localhost:8443";
+    break;
+  default:
+    backendUrl = "http://localhost:9000";
+    break;
+}
+
 export default {
-  backendUrl: process.env.VUE_APP_BACKEND_URL
+  backendUrl
 };
