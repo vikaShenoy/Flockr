e89b73c15128f7a1283fca1dd53db1d66863ed84
Andy Holden <ash102@uclive.ac.nz>
2019-05-04 16:27:14 +1200

    #story[336] Made some tweaks to the dialogs for adding and viewing photos so they work properly. Still blocked by several endpoints but the basic skeleton is there for when I can work on it properly.

---
+++ frontend/src/containers/Profile/Photos/AddPhotoDialog/AddPhotoDialog.vue
@@ -0,0 +1,73 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-dialog
+            id="AddPhotoDialog"
+            v-model="dataDialog"
+            width="80%">
+
+        <template v-slot:activator="{ on }">
+            <v-btn
+                    color="secondary"
+                    v-on="on"
+                    outline
+                    ><v-icon>add</v-icon></v-btn>
+        </template>
+
+        <v-card>
+            Add the add new photo stuff here.
+        </v-card>
+
+    </v-dialog>
+</template>
+
+<script>
+  export default {
+
+    name: "add-photo-dialog",
+
+    data() {
+      return {
+        dataDialog: false
+      }
+    },
+
+    props: {
+      dialog: {
+        type: Boolean,
+        required: true
+      },
+    },
+
+    watch: {
+
+      "dialog": {
+        handler: "onDialogChanged",
+        immediate: true
+      }
+
+    },
+
+    methods: {
+
+      /**
+       * Called when the dataDialog variable is modified.
+       * Emits an event called dialogChangedEvent with a parameter of the dataDialog variable
+       */
+      closeDialog: function () {
+        this.$emit("closeDialog");
+      },
+
+      /**
+       * Called when the dialog prop is changed.
+       * Updates the dataDialog variable to match the dialog prop.
+       */
+      onDialogChanged: function () {
+        this.dataDialog = this.dialog;
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/containers/Profile/Photos/Photos.vue
+++ frontend/src/containers/Profile/Photos/Photos.vue
@@ -1,43 +1,151 @@
 <template>
-  <div>
-    <h3 style="margin-bottom: 4px;margin-top: 4px;">Photos</h3>
+    <div>
+        <h3 style="margin-bottom: 4px;margin-top: 4px;">Photos</h3>
 
-    <v-card id="photos">
-      <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-12" v-for="photo in photos" v-bind:key="photo">
-          <img class="photo" :src="require(`${photo}`)" alt="Photo" />
-        </div>
-      </div>
+        <v-card  id="photos">
+            <v-responsive>
+                <v-container grid-list-sm fluid>
 
-      <v-btn id="add-btn" outline color="secondary"><v-icon>add</v-icon></v-btn>
-    </v-card>
-  </div>
+                    <!-- users photos -->
+                    <v-layout v-if="photos.length" row wrap>
+                        <v-flex sm12 md6 lg4 v-for="photo in photos" v-bind:key="photo">
+                            <v-img class="photo clickable" v-on:click.stop="openViewPhotoDialog(photo)" :src="getThumbnailPhotoEndpoint(photo.filename)"></v-img>
+                        </v-flex>
+                    </v-layout>
+
+                    <!-- default photos -->
+                    <v-layout v-else row wrap>
+                        <v-spacer align="center">
+                            <h3 class="font-weight-regular">This user has no photos.</h3>
+                        </v-spacer>
+                    </v-layout>
+
+                </v-container>
+            </v-responsive>
+
+            <v-card-actions>
+                <v-spacer align="center">
+                    <add-photo-dialog
+                            :dialog="addPhotoDialog"
+                            v-on:closeDialog="closeAddPhotoDialog"
+                    ></add-photo-dialog>
+                </v-spacer>
+            </v-card-actions>
+        </v-card>
+
+        <view-photo-dialog
+            :dialog="viewPhotoDialog"
+            :photo="currentPhoto"
+            v-on:closeDialog="closeViewPhotoDialog"
+        ></view-photo-dialog>
+
+    </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      photos: ["./beach-holiday.jpg", "./desert-holiday.jpeg", "./snow-holiday.jpg"]
-    };
-  } 
-}
+  import AddPhotoDialog from "./AddPhotoDialog/AddPhotoDialog";
+  import {endpoint} from "../../../utils/endpoint";
+  import {getPhotos} from "./PhotosService";
+  import ViewPhotoDialog from "./ViewPhotoDialog/ViewPhotoDialog";
+
+  export default {
+
+    components: {
+      AddPhotoDialog,
+      ViewPhotoDialog
+    },
+
+    data() {
+      return {
+        addPhotoDialog: false,
+        photos: [],
+        userId: null,
+        currentPhoto: null,
+        viewPhotoDialog: false
+      };
+    },
+
+    methods: {
+
+      /**
+       * Gets a list of photos for this user from the backend and adds it to the photos array.
+       */
+      getUserPhotos: async function () {
+        try {
+          this.photos = await getPhotos(this.userId, 6);
+        } catch (error) {
+          if (error.status === 401) {
+            // Need to log in (pop up then this.$router.push('/')
+          } else if (error.status === 404) {
+            // User doesn't exist (pop up then this.$router.back(); ???
+          }
+        }
+      },
+
+      /**
+       * Gets the photo thumbnail endpoint to query for the given photo filename
+       *
+       * @param photoFilename the filename of the photo
+       * @return String the url of photos the endpoint
+       */
+      getThumbnailPhotoEndpoint: function (photoFilename) {
+        return endpoint(`/travellers/photos/${photoFilename}/thumbnail`);
+      },
+
+      /**
+       * Called when a photo is clicked on.
+       * Sets the current photo as the one clicked on.
+       * Opens the view photo dialog by setting viewPhotoDialog to true
+       *
+       * @param photo the photo clicked on by the user
+       */
+      openViewPhotoDialog: function (photo) {
+        this.currentPhoto = photo;
+        this.viewPhotoDialog = true;
+      },
+
+      /**
+       * Called when the view photo dialog is closed by the user.
+       * Sets the current photo to null again.
+       * Closes the view photo dialog by setting viewPhotoDialog to false
+       */
+      closeViewPhotoDialog: function () {
+        this.currentPhoto = null;
+        this.viewPhotoDialog = false;
+      },
+
+      /**
+       * Called when the add photo dialog is closed by the user.
+       * Closes the add photo dialog by setting addPhotoDialog to false.
+       */
+      closeAddPhotoDialog: function () {
+        this.addPhotoDialog = false;
+      }
+
+    },
+
+    mounted: function () {
+      this.userId = this.$route.params.id;
+      this.photos = this.getUserPhotos();
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-  #photos {
-    padding: 20px;
-  }
 
-  .photo {
-    width: 100%;
-    height: auto;
-  }
+    #photos {
+        padding: 20px;
+    }
 
-  #add-btn {
-    display: block;
-    margin: 0 auto;
-  }
+    .photo {
+        width: 100%;
+        height: auto;
+    }
+
+    .clickable :hover {
+        cursor: pointer;
+    }
+
 </style>
 
 
