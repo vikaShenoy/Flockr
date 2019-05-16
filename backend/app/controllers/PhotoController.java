5bcf3abc196d46129eeeff300415babd2cff0bea
Angelica Dela Cruz <acd66@uclive.ac.nz>
2019-05-16 23:51:11 +1200

    Implement the method that is responsible for changing the permission groups of the given photo id.
    - Ensures that the user is either the admin or the owner of the photo
    - Ensures that the photo exists, if not throws a not found exception
    - Checks if the given JSON is the same as the specified in the API spec
    
    #implement #story[387]

--- backend/app/controllers/PhotoController.java
+++ backend/app/controllers/PhotoController.java
@@ -1,38 +1,189 @@
 package controllers;
 
+import actions.ActionState;
+import actions.LoggedIn;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import exceptions.NotFoundException;
+import exceptions.UnauthorizedException;
+import models.PersonalPhoto;
+import models.User;
 import play.libs.Files;
 import play.libs.Json;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.mvc.With;
 import repository.PhotoRepository;
+import repository.UserRepository;
+import util.PhotoUtil;
 import util.Security;
 
 import javax.inject.Inject;
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.FileSystem;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
-import static play.mvc.Results.badRequest;
-import static play.mvc.Results.internalServerError;
-import static play.mvc.Results.unauthorized;
+import static play.mvc.Results.*;
+
 
 public class PhotoController {
     private final PhotoRepository photoRepository;
     private HttpExecutionContext httpExecutionContext;
+    private UserRepository userRepository;
+    private PhotoUtil photoUtil;
 
     @Inject
-    public PhotoController(PhotoRepository photoRepository, HttpExecutionContext httpExecutionContext) {
+    public PhotoController(PhotoRepository photoRepository, UserRepository userRepository, PhotoUtil photoUtil, HttpExecutionContext httpExecutionContext) {
         this.photoRepository = photoRepository;
         this.httpExecutionContext = httpExecutionContext;
+        this.userRepository = userRepository;
+        this.photoUtil = photoUtil;
+    }
+
+
+    /**
+     * This function is responsible for changing the permissions of a photo to either a private or a public.
+     *
+     * @param photoId the id of the photo to be changed
+     * @param request the Http request
+     * @return a completion stage and a status code 200 if the request is successful, otherwise returns 500.
+     */
+    @With(LoggedIn.class)
+    public CompletionStage<Result> updatePermission(int photoId, Http.Request request) {
+        User user = request.attrs().get(ActionState.USER);
+        JsonNode requestBody = request.body().asJson();
+        ObjectNode response = Json.newObject();
+
+        // If the request body does not contain is public key
+        if (!requestBody.has("isPublic")) {
+            response.put("message", "Please specify the JSON body as specified in the API spec");
+            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+        }
+
+        // If the JSON is empty
+        if (requestBody.size() == 0) {
+            response.put("message", "The request body is empty. Please specify the JSON body as " +
+                    "specified in the API spec");
+            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+        }
+
+        boolean isPublic = requestBody.asBoolean();
+        return photoRepository.getPhotoById(photoId)
+                .thenApplyAsync(optionalPhoto -> {
+                    // If the photo with the given photo id does not exists
+                    if (!optionalPhoto.isPresent()) {
+                        throw new CompletionException(new NotFoundException());
+                    }
+
+                    // Checks that the user is either the admin or the owner of the photo to change permission groups
+                    if (!user.isAdmin() && user.getUserId() != optionalPhoto.get().getUser().getUserId()) {
+                        throw new CompletionException(new UnauthorizedException());
+                    }
+
+                    PersonalPhoto photo = optionalPhoto.get();
+                    photo.setPermission(isPublic);
+                    return photoRepository.updatePhoto(photo);
+                }).thenApplyAsync(PersonalPhoto -> ok("Successfully updated permission groups"))
+                .exceptionally(e -> {
+                    try {
+                        throw e.getCause();
+                    } catch (NotFoundException notFoundException) {
+                        response.put("message", "Could not find a photo with the given user id and photo id");
+                        return notFound(response);
+                    } catch (UnauthorizedException unauthorizedException) {
+                        response.put("message", "You are unauthorised to change the photo permission");
+                        return forbidden(response);
+                    } catch (Throwable serverException) {
+                        response.put("message", "Endpoint under development");
+                        return internalServerError(response);
+                    }
+                });
+    }
+
+
+    /**
+     * This function is responsible for deleting the photo with the given ID
+     *
+     * @param photoFilename the hashed filename of the photo to be deleted
+     * @param request       the Http request sent
+     * @return a Play result
+     */
+    @With(LoggedIn.class)
+    public CompletionStage<Result> deletePhoto(String photoFilename, Http.Request request) {
+        return photoRepository.getPhotoByFilename(photoFilename)
+                .thenComposeAsync((optionalPhoto) -> {
+                    if (!optionalPhoto.isPresent()) {
+                        throw new CompletionException(new NotFoundException());
+                    }
+                    PersonalPhoto photo = optionalPhoto.get();
+                    ObjectNode message = Json.newObject();
+                    message.put("message", "Successfully deleted the given filename photo");
+                    return this.photoRepository.deletePhoto(photo.getPhotoId());
+                })
+                .thenApplyAsync(photoId -> (Result) ok())
+                .exceptionally(e -> {
+                    try {
+                        throw e.getCause();
+                    } catch (NotFoundException error) {
+                        ObjectNode message = Json.newObject();
+                        message.put("message", "The photo with the given hashed filename is not found");
+                        return notFound(message);
+                    } catch (Throwable serverError) {
+                        return internalServerError();
+                    }
+                });
+    }
+
+    /**
+     * Gets all photos of a given user using the userId as a reference.
+     *
+     * @param userId  the id of the user
+     * @param request the http request
+     * @return a completion stage and a status code 200.
+     */
+    @With(LoggedIn.class)
+    public CompletionStage<Result> getPhotos(int userId, Http.Request request) {
+        return photoRepository.getPhotosById(userId)
+                .thenApplyAsync((photos) -> {
+                    JsonNode photosAsJSON = Json.toJson(photos);
+                    System.out.println(photosAsJSON.asText());
+                    return ok(photosAsJSON);
+                });
+    }
+
+    /**
+     * Gets a specific photo
+     *
+     * @param photoId the id of the photo to retrieve
+     * @param request       HTTP request object
+     * @return binary photo data with status 200 if found
+     *         unauthorized with 401 if not authorized
+     *         notFound with 404 if photo not found //TODO Add this to the API spec!
+     *         forbidden 403 if trying to get a photo that you do not have permission to
+     *         500 server error for any other server related error
+     */
+    @With(LoggedIn.class)
+    public CompletionStage<Result> getPhoto(int photoId, Http.Request request) {
+
+        User user = request.attrs().get(ActionState.USER);
+
+        return photoRepository.getPhotoById(photoId).thenApplyAsync(optionalPhoto -> {
+            if (!optionalPhoto.isPresent()) {
+                return notFound();
+            } else{
+
+                if (!user.isAdmin() && !optionalPhoto.get().getIsPublic() && user.getUserId() != optionalPhoto.get().getUser().getUserId()) {
+                    return forbidden();
+                } else {
+                    return ok().sendFile(new File("./app/photos/" + optionalPhoto.get().getFileNameHash()));
+                }
+            }
+        });
+
     }
 
     /**
---
+++ backend/app/models/PersonalPhoto.java
@@ -0,0 +1,82 @@
+package models;
+
+import io.ebean.Finder;
+import io.ebean.Model;
+import play.data.validation.Constraints;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * A personal photo that is linked to a user
+ */
+
+@Entity
+public class PersonalPhoto extends Model {
+
+    @ManyToOne
+    private User user;
+
+    @Id
+    private int photoId;
+
+    @Constraints.Required
+    private boolean isPublic;
+
+    @Constraints.Required
+    private boolean isPrimary;
+
+    @Constraints.Required
+    private String filenameHash;
+
+    /**
+     * Constructor
+     * @param filenameHash the hashed filename of the photo
+     * @param isPublic true if the photo is public, otherwise photo is private
+     * @param user the user that the photo is associated to
+     * @param isPrimary true if the photo is the primary photo of the user, otherwise false
+     */
+    public PersonalPhoto(String filenameHash, boolean isPublic, User user, boolean isPrimary) {
+        this.filenameHash = filenameHash;
+        this.isPublic = isPublic;
+        this.user = user;
+        this.isPrimary = isPrimary;
+    }
+
+    public boolean getIsPublic() {
+        return this.isPublic;
+    }
+    public int getPhotoId() {
+        return photoId;
+    }
+
+    public String getFileNameHash() {return this.filenameHash;}
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setPermission(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isPrimary() {
+        return isPrimary;
+    }
+
+    public void setPrimary(boolean primary) {
+        isPrimary = primary;
+    }
+
+
+
+    /**
+     * This function allows EBean to make queries on the database
+     */
+    public static final Finder<Integer, PersonalPhoto> find = new Finder<>(PersonalPhoto.class);
+}
