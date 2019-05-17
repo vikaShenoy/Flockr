c3a9c671336654e5d498bacc75d2f21fd89ce031
Exequiel Bahamonde Cárcamo <eba54@uclive.ac.nz>
2019-05-18 07:38:45 +1200

    Check if user uploading photo is authorised

--- backend/app/controllers/PhotoController.java
+++ backend/app/controllers/PhotoController.java
@@ -4,6 +4,7 @@ import actions.ActionState;
 import actions.LoggedIn;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import exceptions.ForbiddenRequestException;
 import exceptions.NotFoundException;
 import exceptions.UnauthorizedException;
 import models.PersonalPhoto;
@@ -30,6 +31,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 public class PhotoController extends Controller {
 
@@ -195,83 +197,133 @@ public class PhotoController extends Controller {
     }
 
     /**
+     * Determine whether the user doing an upload can upload a photo for receiving user
+     * @param uploadingUserId the id of the user doing the upload
+     * @param receivingUserId the if of the user for which the uploaded photo is
+     * @return true (wrapped in a completion stage) if upload is allowed, false (wrapped in a
+     * completion stage) otherwise
+     */
+    private CompletionStage<Boolean> canUserUploadPhoto(int uploadingUserId, int receivingUserId) {
+        return userRepository.getUserById(uploadingUserId)
+            .thenCombineAsync(userRepository.getUserById(receivingUserId), (optionalUploadingUser, optionalReceivingUser) -> {
+                // if either user does not exist, we can't really upload
+                if (!optionalUploadingUser.isPresent() || !optionalReceivingUser.isPresent()) {
+                    return false;
+                }
+
+                User uploadingUser = optionalUploadingUser.get();
+                User receivingUser = optionalReceivingUser.get();
+
+                if (uploadingUser.isAdmin() || uploadingUser.isDefaultAdmin()) {
+                    return true;
+                }
+
+                if (uploadingUser.equals(receivingUser)) {
+                    return true;
+                }
+                return false;
+            }, httpExecutionContext.current());
+    }
+
+    /**
      * Upload a new photo for a user
      * @param userId the id of the user we are trying to add the photo for
      * @param request the HTTP request requesting the upload
      * @return the result for the request
      */
+    @With(LoggedIn.class)
     public CompletionStage<Result> uploadPhotoForUser(int userId, Http.Request request) {
-        Optional<String> optionalContentType = request.contentType();
+        User userUploadingPhoto = request.attrs().get(ActionState.USER);
         ObjectNode response = Json.newObject();
         String messageKey = "message";
 
-        // TODO: check that we are authorised to upload the photo
+        return canUserUploadPhoto(userUploadingPhoto.getUserId(), userId)
+            .thenComposeAsync(canUploadPhoto -> {
+                if (!canUploadPhoto) {
+                    String message = String.format("User %d is not allowed to upload a photo for user %d", userUploadingPhoto.getUserId(), userId);
+                    throw new CompletionException(new ForbiddenRequestException(message));
+                }
 
-        // check that a content type is specified
-        if (!optionalContentType.isPresent()) {
-            response.put(messageKey, "Please specify the Content Type as specified in the API spec");
-            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
-        }
+                Optional<String> optionalContentType = request.contentType();
 
-        String contentType = optionalContentType.get();
+                // check that a content type is specified
+                if (!optionalContentType.isPresent()) {
+                    response.put(messageKey, "Please specify the Content Type as specified in the API spec");
+                    return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+                }
 
-        // check that the content type is allowed
-        if (!contentType.equals("multipart/form-data")) {
-            response.put(messageKey, "Please specify the Content Type as specified in the API spec");
-            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
-        }
+                String contentType = optionalContentType.get();
 
-        // parse request body to multipart form data
-        Http.MultipartFormData multipartFormData = request.body().asMultipartFormData();
-        Http.MultipartFormData.FilePart photo = multipartFormData.getFile("image");
-        Map<String, String[]> textFields = multipartFormData.asFormUrlEncoded();
+                // check that the content type is allowed
+                if (!contentType.equals("multipart/form-data")) {
+                    response.put(messageKey, "Please specify the Content Type as specified in the API spec");
+                    return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+                }
 
-        // check that the body includes fields as specified in the API spec
-        if (!(textFields.containsKey("isPrimary") || textFields.containsKey("isPublic"))) {
-            response.put(messageKey, "Please provide the request body as specified in the API spec");
-            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
-        }
+                // parse request body to multipart form data
+                Http.MultipartFormData multipartFormData = request.body().asMultipartFormData();
+                Http.MultipartFormData.FilePart photo = multipartFormData.getFile("image");
+                Map<String, String[]> textFields = multipartFormData.asFormUrlEncoded();
 
-        // check that the body includes the photo
-        if (photo == null) {
-            response.put(messageKey, "Missing photo in the request body");
-            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
-        }
+                // check that the body includes fields as specified in the API spec
+                if (!(textFields.containsKey("isPrimary") || textFields.containsKey("isPublic"))) {
+                    response.put(messageKey, "Please provide the request body as specified in the API spec");
+                    return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+                }
 
-        // check for the photo's content type
-        String photoContentType = photo.getContentType();
-        if (!(photoContentType.equals("image/png") || photoContentType.equals("image/jpeg"))) {
-            response.put(messageKey, "Your photo must be a .png or .jpg file");
-            return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
-        }
+                // check that the body includes the photo
+                if (photo == null) {
+                    response.put(messageKey, "Missing photo in the request body");
+                    return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+                }
 
-        // initialise the text components of the request
-        String isPrimaryAsText = textFields.get("isPrimary")[0];
-        String isPublicAsText = textFields.get("isPublic")[0];
-        boolean isPrimary = isPrimaryAsText.equals("true");
-        boolean isPublic = isPublicAsText.equals("true");
+                // check for the photo's content type
+                String photoContentType = photo.getContentType();
+                if (!(photoContentType.equals("image/png") || photoContentType.equals("image/jpeg"))) {
+                    response.put(messageKey, "Your photo must be a .png or .jpg file");
+                    return supplyAsync(() -> badRequest(response), httpExecutionContext.current());
+                }
 
-        // get the photo as a file from the request
-        Files.TemporaryFile temporaryPhotoFile = (Files.TemporaryFile) photo.getRef();
+                // initialise the text components of the request
+                String isPrimaryAsText = textFields.get("isPrimary")[0];
+                String isPublicAsText = textFields.get("isPublic")[0];
+                boolean isPrimary = isPrimaryAsText.equals("true");
+                boolean isPublic = isPublicAsText.equals("true");
 
-        // copy file to local file system
-        String path = System.getProperty("user.dir") + "/storage/photos";
-        Security security = new Security();
-        String token = security.generateToken();
-        String extension = photoContentType.equals("image/png") ? ".png" : ".jpg";
-        String filename = token + extension;
-        File fileDestination = new File(path, filename);
+                // get the photo as a file from the request
+                Files.TemporaryFile temporaryPhotoFile = (Files.TemporaryFile) photo.getRef();
 
-        // if the file path already exists, generate another token
-        while (fileDestination.exists()) {
-            token = security.generateToken();
-            filename = token + extension;
-            fileDestination = new File(path, filename);
-        }
+                // copy file to local file system
+                String path = System.getProperty("user.dir") + "/storage/photos";
+                Security security = new Security();
+                String token = security.generateToken();
+                String extension = photoContentType.equals("image/png") ? ".png" : ".jpg";
+                String filename = token + extension;
+                File fileDestination = new File(path, filename);
 
-        temporaryPhotoFile.moveFileTo(fileDestination);
+                // if the file path already exists, generate another token
+                while (fileDestination.exists()) {
+                    token = security.generateToken();
+                    filename = token + extension;
+                    fileDestination = new File(path, filename);
+                }
 
-        response.put(messageKey, "Endpoint under development");
-        return supplyAsync(() -> internalServerError(response), httpExecutionContext.current());
+                temporaryPhotoFile.moveFileTo(fileDestination);
+
+                response.put(messageKey, "Endpoint under development");
+                return supplyAsync(() -> internalServerError(response), httpExecutionContext.current());
+            }, httpExecutionContext.current())
+            .exceptionally(error -> {
+                try {
+                    throw error.getCause();
+                } catch (ForbiddenRequestException forbiddenRequest) {
+                    response.put(messageKey, forbiddenRequest.getMessage());
+                    return forbidden(response);
+                } catch (Throwable throwable) {
+                    System.err.println("Unexpected error when uploading a photo: " + throwable.getStackTrace());
+                    response.put(messageKey, "Something went wrong trying to upload the photo");
+                    return internalServerError(response);
+                }
+            });
     }
 }
