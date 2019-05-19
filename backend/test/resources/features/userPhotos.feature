08c7d5b7bb2d9fa48b5508e293a529ab73f49604
Andy Holden <ash102@uclive.ac.nz>
2019-05-19 13:21:41 +1200

    #story[387] Completed the tests for get photos by user Id endpoint. All error codes are now tested for as well.

--- backend/test/resources/features/userPhotos.feature
+++ backend/test/resources/features/userPhotos.feature
@@ -1,9 +1,10 @@
 Feature: As a registered user I want to have photos that display on my profile.
 
   Background:
-    Given a user with the following information exists:
+    Given users with the following information exist:
       | firstName | lastName   | email                  | password   |
       | Phillip   | Phototaker | ppt@phillipsphotos.com | say-cheese |
+      | Angelo    | Admin      | aa@admin.com           | its-ya-boi |
 
   @UserPhoto
   Scenario: a user wants to view all of their photos
@@ -15,6 +16,29 @@ Feature: As a registered user I want to have photos that display on my profile.
       | cucumber.jpeg | false     | false    |
       | whale.png     | false     | false    |
     When the user requests all their photos
+    Then the user gets the same list
+
+  @UserPhotos
+  Scenario: a user requests a list of photos for a user that does not exist
+    When a user requests photos for a non signed up userId
+    Then they should receive a "Not Found" error message with a 404 error code
+
+  @UserPhotos
+  Scenario: a user that has not signed in requests a list of their photos
+    When a non signed in user requests photos for another user
+    Then they should receive a "Unauthorized" error message with a 401 error code
+
+  @UserPhotos
+  Scenario: an admin user requests the photos of another user
+    Given one of the users is an admin
+    Given the user has the following photos in the system:
+      | filename      | isPrimary | isPublic |
+      | monkey.png    | true      | false    |
+      | dog.jpg       | false     | false    |
+      | cat.jpeg      | false     | false    |
+      | cucumber.jpeg | false     | false    |
+      | whale.png     | false     | false    |
+    When the admin user requests the photos of another user
     Then the user gets the same list
 
   @UserPhoto
--- backend/test/steps/UserPhotoSteps.java
+++ backend/test/steps/UserPhotoSteps.java
@@ -4,19 +4,25 @@ import akka.stream.javadsl.FileIO;
 import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import controllers.routes;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.cucumber.datatable.DataTable;
 import models.PersonalPhoto;
+import models.Role;
 import models.User;
 import org.junit.Assert;
 import play.Application;
+import play.api.http.HttpEntity;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
 import utils.FakeClient;
+import utils.PlayResultToJson;
 import utils.TestState;
 
 import java.io.File;
@@ -35,7 +41,7 @@ public class UserPhotoSteps {
 
     @Given("^the user has the following photos in the system:$")
     public void theUserHasTheFollowingPhotosInTheSystem(DataTable dataTable) {
-        User testUser = TestState.getInstance().getUser(0);
+        User testUser = TestState.getInstance().removeUser(0);
         this.photoList = dataTable;
         List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
 
@@ -44,15 +50,19 @@ public class UserPhotoSteps {
         List<PersonalPhoto> photos = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             Map<String, String> row = list.get(i);
-            PersonalPhoto photo = new PersonalPhoto(row.get("filename"), user, Boolean.valueOf(row.get("isPublic")), Boolean.valueOf(row.get("isPrimary")));
+            PersonalPhoto photo = new PersonalPhoto(row.get("filename"), Boolean.valueOf(row.get("isPublic")), user, Boolean.valueOf(row.get("isPrimary")));
             photo.save();
+            photo = PersonalPhoto.find.byId(photo.getPhotoId());
             photos.add(photo);
 
-            Assert.assertNotEquals(0, photo.getPhotoId());
         }
         user.setPersonalPhotos(photos);
+        user.save();
+        user = User.find.byId(testUser.getUserId());
+
+        Assert.assertNotNull(user);
         Assert.assertEquals(list.size(), user.getPersonalPhotos().size());
-        user.update();
+        TestState.getInstance().addUser(user);
     }
 
     @When("^the user tries to retrieve their photos$")
@@ -101,7 +111,7 @@ public class UserPhotoSteps {
     }
 
     @When("they add the photo")
-    public void theyAddThePhoto() throws IOException {
+    public void theyAddThePhoto() {
         // TODO: Exe to clean up this mess
 
         User user = TestState.getInstance().getUser(0);
@@ -150,16 +160,73 @@ public class UserPhotoSteps {
 
     @Then("the photo is added")
     public void isItAdded() {
-        Assert.assertEquals(200, result.status());
+        Assert.assertEquals(200, this.result.status());
     }
 
     @When("^the user requests all their photos$")
     public void theUserRequestsAllTheirPhotos() {
+        FakeClient fakeClient = TestState.getInstance().getFakeClient();
+        User user = TestState.getInstance().getUser(0);
 
+        this.result = fakeClient.makeRequestWithToken("GET", "/api/users/" + user.getUserId() + "/photos", user.getToken());
+
+        Assert.assertEquals(200, this.result.status());
     }
 
     @Then("^the user gets the same list$")
-    public void theUserGetsTheSameList() {
+    public void theUserGetsTheSameList() throws IOException {
+        JsonNode jsonNode = PlayResultToJson.convertResultToJson(this.result);
+        Assert.assertTrue(jsonNode.isArray());
+        Iterator<JsonNode> iterator = jsonNode.iterator();
+        Iterator<Map<String, String>> rowsIterator = this.photoList.asMaps().iterator();
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        while (iterator.hasNext()) {
+            JsonNode nextPhoto = iterator.next();
+            PersonalPhoto personalPhoto = objectMapper.treeToValue(nextPhoto, PersonalPhoto.class);
+            Map<String, String> row = rowsIterator.next();
+            Assert.assertEquals(row.get("filename"), personalPhoto.getFileNameHash());
+            Assert.assertEquals(Boolean.valueOf(row.get("isPrimary")), personalPhoto.isPrimary());
+            Assert.assertEquals(Boolean.valueOf(row.get("isPublic")), personalPhoto.isPublic());
+        }
+    }
+
+    @When("^a user requests photos for a non signed up userId$")
+    public void aUserRequestsPhotosForANonSignedUpUserId() {
+        FakeClient fakeClient = TestState.getInstance().getFakeClient();
+        User user = TestState.getInstance().getUser(0);
+        this.result = fakeClient.makeRequestWithToken("GET", "/api/users/500/photos", user.getToken());
+        Assert.assertNotNull(this.result);
+    }
+
+    @When("^a non signed in user requests photos for another user$")
+    public void aUserRequestsPhotosForANonSignedInUserId() {
+        FakeClient fakeClient = TestState.getInstance().getFakeClient();
+        this.result = fakeClient.makeRequestWithNoToken("GET", "/api/users/1/photos");
+        Assert.assertNotNull(this.result);
+    }
+
+    @Then("^they should receive a \"([^\"]*)\" error message with a (\\d+) error code$")
+    public void theyShouldReceiveAErrorMessageWithAErrorCode(String errorMessage, int errorCode) {
+        Assert.assertEquals(errorCode, this.result.status());
+    }
+
+    @Given("^one of the users is an admin$")
+    public void oneOfTheUsersIsAnAdmin() {
+        User admin = TestState.getInstance().getUser(1);
+        List<Role> roles = Role.find.query().where().eq("role_type", "ADMIN").findList();
+        Assert.assertEquals(1, roles.size());
+        admin.setRoles(roles);
+        admin.save();
+    }
+
+    @When("^the admin user requests the photos of another user$")
+    public void theAdminUserRequestsThePhotosOfAnotherUser() {
+        FakeClient fakeClient = TestState.getInstance().getFakeClient();
+        User user = TestState.getInstance().getUser(0);
+        User admin = TestState.getInstance().getUser(1);
+
+        this.result = fakeClient.makeRequestWithToken("GET", "/api/users/" + user.getUserId() + "/photos", admin.getToken());
+        Assert.assertNotNull(this.result);
     }
 }
