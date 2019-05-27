fcb73fc4321d538b55ba4f9dfa9cdde14f2a0f98
Sam Annand <sga111@uclive.ac.nz>
2019-05-27 20:59:35 +1200

    Adding template dialog to be filled in with photos to be added #story[385]

---
+++ frontend/src/containers/Destinations/DestinationCard/Carousel/AddDestinationPhotoDialog/AddDestinationPhotoDialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+
+        <v-dialog v-model="showAddPhotoDialog">
+            <v-card>
+                <v-card-title>
+                    Testing
+                </v-card-title>
+                <v-card-text>
+                    This will be the dialog to link personal photos to a destination
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AddPhotoDialog",
+        props: {
+            showDialog: {
+                type: Boolean,
+                required: true
+            }
+        },
+        data() {
+            return {
+                showAddPhotoDialog: false
+            }
+        },
+        watch: {
+            showDialog: {
+                handler: 'onShowDialogUpdated',
+                immediate: true
+            },
+            showAddPhotoDialog: {
+                handler: 'onShowPhotoDialogUpdated',
+                immediate: true
+            }
+        },
+        methods: {
+            onShowDialogUpdated() {
+                this.showAddPhotoDialog = this.showDialog;
+            },
+            onShowPhotoDialogUpdated() {
+                this.$emit('closeAddPhotoDialog', this.showAddPhotoDialog);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
