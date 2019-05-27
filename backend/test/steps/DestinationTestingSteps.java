3bfee9e01e811ab5064cfa4576d411c2797d2888
Angelica Dela Cruz <acd66@uclive.ac.nz>
2019-05-27 19:03:51 +1200

    Merge branch 'S390-Types-of-Destination' of https://eng-git.canterbury.ac.nz/seng302-2019/team-500 into S390-Types-of-Destination
    # Conflicts:
    #	backend/test/resources/features/destination.feature
    #	backend/test/steps/DestinationTestingSteps.java

--- backend/test/steps/DestinationTestingSteps.java
+++ backend/test/steps/DestinationTestingSteps.java
@@ -1,7 +1,6 @@
 package steps;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import cucumber.api.java.en.Given;
