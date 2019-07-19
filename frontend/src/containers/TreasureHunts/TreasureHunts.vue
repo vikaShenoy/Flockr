78e8093e6f3f75f40815ac59a14f97af47e0a462
Sam Annand <sga111@uclive.ac.nz>
2019-07-19 14:19:58 +1200

    Working on treasure hunts #story[465]

--- frontend/src/containers/TreasureHunts/TreasureHunts.vue
+++ frontend/src/containers/TreasureHunts/TreasureHunts.vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        Dope asf
-    </div>
+    <v-flex>
+          <v-expansion-panel>
+            <v-expansion-panel-content
+            v-for="(item,i) in 5"
+            :key="i"
+            >
+            <template v-slot:header>
+                <div>Item</div>
+            </template>
+            <v-card>
+                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+            </v-card>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-flex>
 </template>
 
 <script>
 export default {
+
+    data() {
+        return {
+            treasureHunts: [
+                {
+                    name: "The curse of the Krusty Krab",
+                    riddle: "Where is the Krusty crab located",
+                    timezone: "GMC+13"
+                }
+            ]
+        }
+    }
     
 }
 </script>
