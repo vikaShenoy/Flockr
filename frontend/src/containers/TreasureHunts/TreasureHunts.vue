bb1297046c748fa0dbd40133591a8331fe71b9d0
Sam Annand <sga111@uclive.ac.nz>
2019-07-21 13:38:46 +1200

    Adding some default treasure hunts #story[465]

--- frontend/src/containers/TreasureHunts/TreasureHunts.vue
+++ frontend/src/containers/TreasureHunts/TreasureHunts.vue
@@ -2,14 +2,14 @@
     <v-flex>
           <v-expansion-panel>
             <v-expansion-panel-content
-            v-for="(item,i) in 5"
+            v-for="(item, i) in treasureHunts"
             :key="i"
             >
             <template v-slot:header>
-                <div>Item</div>
+                <div>{{item.name}}</div>
             </template>
             <v-card>
-                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+                <v-card-text>{{item.riddle}}</v-card-text>
             </v-card>
             </v-expansion-panel-content>
         </v-expansion-panel>
@@ -24,8 +24,27 @@ export default {
             treasureHunts: [
                 {
                     name: "The curse of the Krusty Krab",
-                    riddle: "Where is the Krusty crab located",
-                    timezone: "GMC+13"
+                    riddle: "Where is the Krusty Krab located?",
+                    timezone: "GMC+13",
+                    destination: "Bikini Bottom"
+                },
+                {
+                    name: "Pointy stick",
+                    riddle: "This city has a pointy stick in it that recently had Saturn V projected onto it",
+                    timezone: "GMC+13",
+                    destination: "Washington DC"
+                },
+                {
+                    name: "A place to drink",
+                    riddle: "Live band, a fat man, scissorhands",
+                    timezone: "GMC+13",
+                    destination: "Fat Eddies"
+                },
+                {
+                    name: "Treasure",
+                    riddle: "People live there, work there, study there, cry there, eat there",
+                    timezone: "GMC+13",
+                    destination: "Erskine"
                 }
             ]
         }
