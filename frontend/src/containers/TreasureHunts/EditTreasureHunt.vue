9b76709c56c7ae7b2e37c6cde29c45401abe97dc
Sam Annand <sga111@uclive.ac.nz>
2019-07-23 13:45:40 +1200

    Half-implemented the ability to edit a treasure hunt, focussing on getting the dialog to appear #story[465]

---
+++ frontend/src/containers/TreasureHunts/EditTreasureHunt.vue
@@ -0,0 +1,156 @@
+<template>
+    <v-dialog
+            v-model="visible"
+            width="500"
+    >
+
+        <v-tabs
+                centered
+                color="secondary"
+                dark
+                icons-and-text
+                hide-slider
+        >
+
+            <v-tab href="#tab-1">
+                Edit Treasure Hunt
+                <v-icon>zoom_out_map</v-icon>
+            </v-tab>
+
+            <v-tab-item
+                    key="1"
+                    value="tab-1"
+            >
+                <v-card flat>
+                    <v-card-text>
+                        <v-form
+                                ref="treasureHuntForm"
+                        >
+                            <v-container grid-list-md>
+                                <v-layout wrap>
+
+                                    <v-flex xs12>
+                                        <v-text-field v-model="editTreasureHuntName" label="Name" required ></v-text-field>
+                                    </v-flex>
+                                    <v-flex xs12>
+                                        <v-select v-model="editTreasureHuntDestination" required label="Destination" :items="destinations" item-text="destinationName" item-value="destinationId">
+
+                                        </v-select>
+                                    </v-flex>
+                                    <v-flex xs12>
+                                        <v-textarea v-model="editTreasureHuntRiddle" label="Riddle" required></v-textarea>
+                                    </v-flex>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                                v-model="startDate"
+                                                label="Start Date"
+                                                prepend-icon="event"
+                                                type="date"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                                v-model="endDate"
+                                                label="End Date"
+                                                prepend-icon="event"
+                                                type="date"
+                                        ></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                            </v-container>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat @click="closeDialog">Close</v-btn>
+                        <v-btn color="blue darken-1" :disabled="false" flat v-on:click="createTreasureHunt()" >Update</v-btn>
+                    </v-card-actions>
+                </v-card>
+
+            </v-tab-item>
+        </v-tabs>
+
+
+
+    </v-dialog>
+</template>
+
+<script>
+    import {getPublicDestinations, createTreasureHunt} from "./TreasureHuntsService"
+    export default {
+        name: "EditTreasureHunt",
+        props: {
+            toggle: Boolean,
+            data: Object
+        },
+        mounted() {
+            this.getDestinations();
+            this.editTreasureHuntName = this.data.treasureHuntName;
+            this.editTreasureHuntRiddle = this.data.riddle;
+            this.editTreasureHuntDestination = this.data.treasureHuntDestinationId;
+            this.startDate = this.data.startDate;
+            this.endDate = this.data.endDate;
+            this.visible = this.toggle;
+
+
+        },
+        data() {
+            return {
+                visible: false,
+                destinations: [],
+                editTreasureHuntName: "",
+                editTreasureHuntDestination: -1,
+                editTreasureHuntRiddle: "",
+                startDate: null,
+                endDate: null,
+
+            }
+        },
+        methods: {
+            closeDialog() {
+                this.$emit("closeEditDialog");
+                this.editTreasureHuntName = "";
+                this.editTreasureHuntRiddle = "";
+                this.editTreasureHuntDestination = null;
+                this.startDate = null;
+                this.endDate = null;
+            },
+            async getDestinations() {
+                this.destinations = await getPublicDestinations()
+            },
+            async editTreasureHunt() {
+                console.log("Create the treasure hunt in this function");
+                let treasureHunt = {
+
+                    treasureHuntName: this.createTreasureHuntName,
+                    treasureHuntDestinationId: this.createTreasureHuntDestination,
+                    riddle: this.createTreasureHuntRiddle,
+                    startDate: this.startDate,
+                    endDate: this.endDate
+
+                };
+                await createTreasureHunt(treasureHunt);
+
+                this.closeDialog();
+                this.$emit("updateList");
+            }
+        },
+        computed: {
+            /*validTreasureHunt() {
+                return ! ( this.editTreasureHuntName.length > 0 && this.editTreasureHuntDestination != null && this.editTreasureHuntRiddle.length > 0 && this.startDate != null && this.endDate != null)
+            }*/
+        },
+        watch: {
+            toggle(newVal, oldVal) {
+                this.visible = newVal
+            },
+            visible(newVal, oldVal) {
+                this.$emit("updateToggle", newVal)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/containers/TreasureHunts/TreasureHunts.vue
+++ frontend/src/containers/TreasureHunts/TreasureHunts.vue
@@ -10,7 +10,7 @@
             <template v-slot:header>
                 <div id="header">{{item.treasureHuntName}}
                     <v-btn :visible="true" color="secondary" style="" fab small
-                           v-on:hover="showDialog = true" v-if="isOwner(item.ownerId)"
+                           v-if="isOwner(item.ownerId)" v-on:click="showEditDialog(item)"
                     >
                         <v-icon>edit</v-icon>
                     </v-btn>
@@ -36,6 +36,13 @@
                 @updateToggle="updateToggle"
                 @updateList="updateList"
         ></AddTreasureHunt>
+
+        <EditTreasureHunt
+                :toggle="showEditForm"
+                :data="treasureHunt"
+                :key="refreshEditForm"
+                @closeEditDialog="closeEditDialog"
+        ></EditTreasureHunt>
     </v-flex>
 
 
@@ -43,48 +50,30 @@
 
 <script>
 import AddTreasureHunt from "./AddTreasureHunt";
+import EditTreasureHunt from "./EditTreasureHunt"
 import {getAllTreasureHunts} from "./TreasureHuntsService";
 import moment from "moment";
 
 export default {
-    components: {AddTreasureHunt},
+    components: {AddTreasureHunt, EditTreasureHunt},
     mounted() {
         this.getTreasureHunts();
     },
     data() {
         return {
             showDialog: false,
-            treasureHunts: [
-                {
-                    name: "The curse of the Krusty Krab",
-                    riddle: "Where is the Krusty Krab located?",
-                    timezone: "GMC+13",
-                    destination: "Bikini Bottom"
-                },
-                {
-                    name: "Pointy stick",
-                    riddle: "This city has a pointy stick in it that recently had Saturn V projected onto it",
-                    timezone: "GMC+13",
-                    destination: "Washington DC"
-                },
-                {
-                    name: "A place to drink",
-                    riddle: "Live band, a fat man, scissorhands",
-                    timezone: "GMC+13",
-                    destination: "Fat Eddies"
-                },
-                {
-                    name: "Treasure",
-                    riddle: "People live there, work there, study there, cry there, eat there",
-                    timezone: "GMC+13",
-                    destination: "Erskine"
-                }
-            ]
+            showEditForm: false,
+            treasureHunts: [],
+            treasureHunt: {},
+            refreshEditForm: 0
         }
     },
     methods: {
         closeDialog() {
             this.showDialog = false
+        },
+        closeEditDialog() {
+            this.showEditForm = false
         },
         async getTreasureHunts() {
             this.treasureHunts = await getAllTreasureHunts();
@@ -112,6 +101,11 @@ export default {
         },
         updateList() {
             this.getTreasureHunts();
+        },
+        showEditDialog(treasureHunt) {
+            this.treasureHunt = treasureHunt;
+            this.refreshEditForm += 1;
+            this.showEditForm = true;
         }
     }
 
