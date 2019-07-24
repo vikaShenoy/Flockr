375deb9735fca33210b2478319cfbd660534dac7
Finn Greig <fgr27@uclive.ac.nz>
2019-07-24 17:45:38 +1200

    Finished Country component implementation and added styling to the CountryDisplay #story[466]

--- frontend/src/components/Country/CountryService.js
+++ frontend/src/components/Country/CountryService.js
@@ -1,7 +1,9 @@
 import superagent from "superagent";
+import {endpoint} from '../../utils/endpoint';
 
 // TODO: get from the backend API endpoint
 export async function getCountries() {
-    const res = await superagent.get("https://restcountries.eu/rest/v2/all");
+    const res = await superagent.get(endpoint('/destinations/countries'))
+        .set("Authorization", localStorage.getItem("authToken"));
     return res.body;
 }
