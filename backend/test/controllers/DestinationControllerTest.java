3b6c73cf92744c9c70163a0e79ae8b8ee33047a9
Angelica Dela Cruz <acd66@uclive.ac.nz>
2019-07-28 01:01:31 +1200

    Tested the methods that is being used for the destination proposal functionalities to ensure they are giving the correct and expected outputs
    #implement #story[467] #test

--- backend/test/controllers/DestinationControllerTest.java
+++ backend/test/controllers/DestinationControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import play.Application;
+import play.libs.Json;
 import play.mvc.Result;
 import play.test.Helpers;
 import utils.FakeClient;
@@ -29,6 +30,7 @@ public class DestinationControllerTest {
     User otherUser;
     User adminUser;
     Destination destination;
+    DestinationProposal destinationProposal;
 
     @Before
     public void setUp() throws ServerErrorException, IOException, FailedToSignUpException {
@@ -72,6 +74,13 @@ public class DestinationControllerTest {
         country.save();
         district.save();
         destination.save();
+
+        // Add some proposal
+        TravellerType travellerType = new TravellerType("Gap Year");
+        List<TravellerType> travellerTypes = new ArrayList<>();
+        travellerTypes.add(travellerType);
+        destinationProposal = new DestinationProposal(destination, travellerTypes, user);
+        destinationProposal.save();
     }
 
     @After
@@ -146,5 +155,137 @@ public class DestinationControllerTest {
                     .where().eq("destination_id", destination.getDestinationId()).findOneOrEmpty();
             Assert.assertTrue(optionalDestination.isPresent());
         }
+    }
+
+    @Test
+    public void undoDeleteProposalGoodUser() {
+        undoDeleteDestinationProposal(user.getToken(), destinationProposal.getDestinationProposalId(), 200, true);
+    }
+
+    @Test
+    public void undoDeleteProposalAdmin() {
+        undoDeleteDestinationProposal(adminUser.getToken(), destinationProposal.getDestinationProposalId(), 200, true);
+    }
+
+    @Test
+    public void undoDeleteProposalForbidden() {
+        undoDeleteDestinationProposal(otherUser.getToken(), destinationProposal.getDestinationProposalId(), 403, true);
+    }
+
+    @Test
+    public void undoDeleteProposalUnauthorised() {
+        Result result = fakeClient.makeRequestWithNoToken(
+                "PUT",
+                "/api/destinations/proposals/" + destinationProposal.getDestinationProposalId() + "/undoReject");
+        Assert.assertEquals(401, result.status());
+
+    }
+
+    @Test
+    public void undoDeleteProposalNotFound() {
+        undoDeleteDestinationProposal(user.getToken(), 999999999, 404, true);
+    }
+
+    @Test
+    public void undoDeleteProposalBadRequest() {
+        undoDeleteDestinationProposal(user.getToken(), destinationProposal.getDestinationProposalId(), 400, false);
+    }
+
+
+    private void undoDeleteDestinationProposal(String token, int destinationProposalId, int statusCode, boolean deleted) {
+        if (deleted) {
+            destinationProposal.delete();
+
+            Optional<DestinationProposal> optionalDestinationProposal = DestinationProposal.find.query()
+                    .where().eq("destination_proposal_id", destinationProposalId).findOneOrEmpty();
+            Assert.assertFalse(optionalDestinationProposal.isPresent());
+        }
+
+        Result result = fakeClient.makeRequestWithToken(
+                "PUT",
+                "/api/destinations/proposals/" + destinationProposalId + "/undoReject",
+                token);
+        Assert.assertEquals(statusCode, result.status());
+
+        if (statusCode == 200) {
+            Optional<DestinationProposal> optionalDestinationProposal = DestinationProposal.find.query()
+                    .where().eq("destination_proposal_id", destinationProposalId).findOneOrEmpty();
+            Assert.assertTrue(optionalDestinationProposal.isPresent());
+        }
+    }
+
+    @Test
+    public void getProposalAdmin()  {
+        getProposalById(adminUser.getToken(), destinationProposal.getDestinationProposalId(), 200);
+    }
+
+    @Test
+    public void getProposalUnauthorised() {
+        getProposalById(user.getToken(), destinationProposal.getDestinationProposalId(), 401);
+    }
+
+    @Test
+    public void getProposalNotFound() {
+        getProposalById(adminUser.getToken(), 9999999, 404);
+    }
+
+    private void getProposalById(String token, int destinationProposalId, int statusCode) {
+        Result result = fakeClient.makeRequestWithToken(
+                "GET",
+                "/api/destinations/proposals/" + destinationProposalId,
+                token);
+        Assert.assertEquals(statusCode, result.status());
+    }
+
+    @Test
+    public void rejectProposalGoodUser() {
+        rejectProposal(user.getToken(), destinationProposal.getDestinationProposalId(), user.getUserId(), 200);
+    }
+
+    @Test
+    public void rejectProposalAdmin() {
+        rejectProposal(adminUser.getToken(), destinationProposal.getDestinationProposalId(), adminUser.getUserId(), 200);
+    }
+
+    @Test
+    public void rejectProposalUnauthorised() {
+        Result result = fakeClient.makeRequestWithNoToken(
+                "DELETE",
+                "/api/users/" + user.getUserId() + "/destinations/proposals/" + destinationProposal.getDestinationProposalId());
+        Assert.assertEquals(401, result.status());
+    }
+
+    @Test
+    public void rejectProposalForbidden() {
+        rejectProposal(otherUser.getToken(), destinationProposal.getDestinationProposalId(), user.getUserId(), 403);
+    }
+
+    private void rejectProposal(String token, int destinationProposalId, int userId, int statusCode) {
+        Result result = fakeClient.makeRequestWithToken(
+                "DELETE",
+                "/api/users/" + userId + "/destinations/proposals/" + destinationProposalId,
+                token);
+        Assert.assertEquals(statusCode, result.status());
+    }
+
+    @Test
+    public void acceptProposalsAdmin() {
+        acceptProposals(adminUser.getToken(), destinationProposal.getDestinationProposalId(), 200);
+        Optional<DestinationProposal> optionalDestinationProposal = DestinationProposal.find.query()
+                .where().eq("destination_proposal_id", destinationProposal.getDestinationProposalId()).findOneOrEmpty();
+        Assert.assertFalse(optionalDestinationProposal.isPresent());
+    }
+
+    @Test
+    public void acceptProposalsUnauthorised() {
+        acceptProposals(user.getToken(), destinationProposal.getDestinationProposalId(), 401);
+    }
+
+    public void acceptProposals(String token, int destinationProposalId, int statusCode) {
+        Result result = fakeClient.makeRequestWithToken(
+                "PATCH",
+                "/api/destinations/proposals/" + destinationProposalId,
+                token);
+        Assert.assertEquals(statusCode, result.status());
     }
 }
