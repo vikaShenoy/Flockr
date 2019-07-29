c6f1e131e1dd8a29b4350f48fffa138020160b0a
Andy Holden <ash102@uclive.ac.nz>
2019-07-29 13:44:53 +1200

    #story[467] #fix wiped the ebean evolutions script as it was causing tests to fail.

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -1,65 +1,8 @@
-<<<<<<< Updated upstream
 # --- Created by Ebean DDL
 # To stop Ebean DDL generation, remove this comment and start using Evolutions
 
 # --- !Ups
 
--- init script create procs
--- Inital script to create stored procedures etc for mysql platform
-DROP PROCEDURE IF EXISTS usp_ebean_drop_foreign_keys;
-
-delimiter $$
---
--- PROCEDURE: usp_ebean_drop_foreign_keys TABLE, COLUMN
--- deletes all constraints and foreign keys referring to TABLE.COLUMN
---
-CREATE PROCEDURE usp_ebean_drop_foreign_keys(IN p_table_name VARCHAR(255), IN p_column_name VARCHAR(255))
-BEGIN
-  DECLARE done INT DEFAULT FALSE;
-  DECLARE c_fk_name CHAR(255);
-  DECLARE curs CURSOR FOR SELECT CONSTRAINT_NAME from information_schema.KEY_COLUMN_USAGE
-    WHERE TABLE_SCHEMA = DATABASE() and TABLE_NAME = p_table_name and COLUMN_NAME = p_column_name
-      AND REFERENCED_TABLE_NAME IS NOT NULL;
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-  OPEN curs;
-
-  read_loop: LOOP
-    FETCH curs INTO c_fk_name;
-    IF done THEN
-      LEAVE read_loop;
-    END IF;
-    SET @sql = CONCAT('ALTER TABLE ', p_table_name, ' DROP FOREIGN KEY ', c_fk_name);
-    PREPARE stmt FROM @sql;
-    EXECUTE stmt;
-  END LOOP;
-
-  CLOSE curs;
-END
-$$
-
-DROP PROCEDURE IF EXISTS usp_ebean_drop_column;
-
-delimiter $$
---
--- PROCEDURE: usp_ebean_drop_column TABLE, COLUMN
--- deletes the column and ensures that all indices and constraints are dropped first
---
-CREATE PROCEDURE usp_ebean_drop_column(IN p_table_name VARCHAR(255), IN p_column_name VARCHAR(255))
-BEGIN
-  CALL usp_ebean_drop_foreign_keys(p_table_name, p_column_name);
-  SET @sql = CONCAT('ALTER TABLE ', p_table_name, ' DROP COLUMN ', p_column_name);
-  PREPARE stmt FROM @sql;
-  EXECUTE stmt;
-END
-$$
-=======
-# --- Created by Ebean DDL
-# To stop Ebean DDL generation, remove this comment and start using Evolutions
-
-# --- !Ups
-
->>>>>>> Stashed changes
 create table country (
   country_id                    integer auto_increment not null,
   country_name                  varchar(255),
@@ -206,7 +149,7 @@ create table trip (
   trip_id                       integer auto_increment not null,
   user_user_id                  integer,
   trip_name                     varchar(255),
-  deleted_expiry                datetime(6),
+  deleted_expiry                timestamp,
   deleted                       BOOLEAN DEFAULT FALSE not null,
   constraint pk_trip primary key (trip_id)
 );
@@ -332,19 +275,11 @@ alter table trip_destination add constraint fk_trip_destination_destination_dest
 
 alter table user add constraint fk_user_profile_photo_photo_id foreign key (profile_photo_photo_id) references personal_photo (photo_id) on delete restrict on update restrict;
 
-<<<<<<< Updated upstream
-
-# --- !Downs
-
-alter table destination drop foreign key fk_destination_destination_type_destination_type_id;
-drop index ix_destination_destination_type_destination_type_id on destination;
-=======
 
 # --- !Downs
 
 alter table destination drop constraint if exists fk_destination_destination_type_destination_type_id;
 drop index if exists ix_destination_destination_type_destination_type_id;
->>>>>>> Stashed changes
 
 alter table destination drop constraint if exists fk_destination_destination_district_district_id;
 drop index if exists ix_destination_destination_district_district_id;
