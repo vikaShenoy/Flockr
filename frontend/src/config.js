4cae421a55a74815c3eec027376c52b4e306d2c4
Andy Holden <ash102@uclive.ac.nz>
2019-07-31 12:56:12 +1200

    #fix Changed backend to localhost because VM is down.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,20 +1,21 @@
 const env = process.env.NODE_ENV;
-const REMOTE_URL = "http://csse-s302g5.canterbury.ac.nz";
+// const REMOTE_URL = "http://csse-s302g5.canterbury.ac.nz";
 const GOOGLE_MAPS_KEY = "AIzaSyDCyC_ON0GgSmJIfPlL7oke5PZgbR8TLbg";
 
-let backendUrl = "";
-
-switch (env) {
-  case "production":
-    backendUrl = `${REMOTE_URL}:443`;
-    break;
-  case "staging":
-    backendUrl = `${REMOTE_URL}:8443`;
-    break;
-  default:
-    backendUrl = "http://localhost:9000";
-    break;
-}
+// let backendUrl = "";
+//
+// switch (env) {
+//   case "production":
+//     backendUrl = `${REMOTE_URL}:443`;
+//     break;
+//   case "staging":
+//     backendUrl = `${REMOTE_URL}:8443`;
+//     break;
+//   default:
+//     backendUrl = "http://localhost:9000";
+//     break;
+// }
+let backendUrl = "http://localhost:9000";  //Temp while VM is down
 
 export default {
   backendUrl,
