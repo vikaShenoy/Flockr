c4f17ebc1464e05378deefd65e3031aeacf9d56d
Andy Holden <ash102@uclive.ac.nz>
2019-07-31 16:55:47 +1200

    #fix Changed front end port

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,20 +1,22 @@
-const env = process.env.NODE_ENV;
-const REMOTE_URL = "http://csse-s302g5.canterbury.ac.nz";
+// const env = process.env.NODE_ENV;
+// const REMOTE_URL = "http://csse-s302g5.canterbury.ac.nz";
 const GOOGLE_MAPS_KEY = "AIzaSyDCyC_ON0GgSmJIfPlL7oke5PZgbR8TLbg";
 
-let backendUrl = "";
+// let backendUrl = "";
+//
+// switch (env) {
+//   case "production":
+//     backendUrl = `${REMOTE_URL}:443`;
+//     break;
+//   case "staging":
+//     backendUrl = `${REMOTE_URL}:8443`;
+//     break;
+//   default:
+//     backendUrl = "http://localhost:9000";
+//     break;
+// }
 
-switch (env) {
-  case "production":
-    backendUrl = `${REMOTE_URL}:443`;
-    break;
-  case "staging":
-    backendUrl = `${REMOTE_URL}:8443`;
-    break;
-  default:
-    backendUrl = "http://localhost:9000";
-    break;
-}
+let backendUrl = "http://localhost:9000";
 
 export default {
   backendUrl,
