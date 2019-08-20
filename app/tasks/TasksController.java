d0fb751814e8b331f57f2ddca951e69910cc7556
Andy Holden <ash102@uclive.ac.nz>
2019-08-20 13:11:06 +1200

    #story[510] #fix pair[ash102,ifo18] Added setup task to add extra column when using an h2 database for child_index.

--- app/tasks/TasksController.java
+++ app/tasks/TasksController.java
@@ -1,36 +1,34 @@
 package tasks;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import play.api.*;
 
 /**
  * Registers the tasks that need to be handled by the application.
  */
 public class TasksController extends AbstractModule {
 
+  @Override
+  protected void configure() {
+    bind(DeleteExpiredPhotosTask.class).asEagerSingleton();
+    bind(DeleteExpiredUsersTask.class).asEagerSingleton();
+    bind(DeleteExpiredDestinationsTask.class).asEagerSingleton();
+    bind(DeleteExpiredDestinationProposal.class).asEagerSingleton();
+    bind(CountrySyncTask.class).asEagerSingleton();
+    bind(DeleteExpiredTripsTask.class).asEagerSingleton();
+    bind(DeleteExpiredTreasureHunts.class).asEagerSingleton();
+    bind(DeleteExpiredDestinationPhotos.class).asEagerSingleton();
+    bind(SetupTask.class).asEagerSingleton();
 
-    @Override
-    protected void configure() {
-        bind(DeleteExpiredPhotosTask.class).asEagerSingleton();
-        bind(DeleteExpiredUsersTask.class).asEagerSingleton();
-        bind(DeleteExpiredDestinationsTask.class).asEagerSingleton();
-        bind(DeleteExpiredDestinationProposal.class).asEagerSingleton();
-        bind(CountrySyncTask.class).asEagerSingleton();
-        bind(DeleteExpiredTripsTask.class).asEagerSingleton();
-        bind(DeleteExpiredTreasureHunts.class).asEagerSingleton();
-        bind(DeleteExpiredDestinationPhotos.class).asEagerSingleton();
+    Config conf = ConfigFactory.load();
+    String environment = conf.getString("environment");
+    System.out.println(environment);
+    // System.out.println(Play.current().configuration().get());
+    // if (environment.equals("dev")) {
+    bind(PopulateTask.class).asEagerSingleton();
+    // }
 
-        Config conf = ConfigFactory.load();
-        String environment = conf.getString("environment");
-        System.out.println(environment);
-        //System.out.println(Play.current().configuration().get());
-        //if (environment.equals("dev")) {
-        bind(PopulateTask.class).asEagerSingleton();
-        //}
-
-        // you may add more tasks here
-    }
+    // you may add more tasks here
+  }
 }
