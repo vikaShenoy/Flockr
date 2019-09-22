5e49e686964b921d23000546a3f7d328a6ac7187
ifo18 <ifo18@uclive.ac.nz>
2019-09-23 10:48:35 +1200

    #story[599] Started 404 page #implement

---
+++ frontend/src/containers/PageNotFound/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="container">
+    <h1>FAW-OH-FAW</h1>
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "PageNotFound.vue"
+  }
+</script>
+
+<style lang="scss" scoped>
+  #container {
+    display: flex;
+    background-image: url("../../assets/background.jpg");
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
+</style>
