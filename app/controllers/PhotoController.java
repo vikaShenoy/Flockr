d5b7067cc4453d427543c999233b9aaef22f7839
Andy Holden <ash102@uclive.ac.nz>
2019-09-23 12:18:15 +1200

    #story[599] modified print for debug on env variables

--- app/controllers/PhotoController.java
+++ app/controllers/PhotoController.java
@@ -516,7 +516,8 @@ public class PhotoController extends Controller {
    */
   public boolean checkForAndCreatePhotosDirectory() {
       String path = System.getProperty("user.dir") + "/storage";
-      System.out.println(System.getenv("VUE_APP_GOOGLE_MAP_API"));
+      System.out.println(System.getenv().toString());
+      System.out.println(path);
       File file = new File(path);
       if (!file.exists() && file.mkdir()) {
           file = new File(path + "/photos");
