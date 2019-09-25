12f053bef2458b07061c109bf8b5ae669157ed07
Andy Holden <ash102@uclive.ac.nz>
2019-09-25 21:15:41 +1200

    #story[599] #fix Fixed missing import in role repository.

--- app/repository/RoleRepository.java
+++ app/repository/RoleRepository.java
@@ -1,7 +1,7 @@
-
 package repository;
 
 import models.Role;
+import models.RoleType;
 import models.User;
 import play.db.ebean.EbeanConfig;
 
@@ -11,43 +11,40 @@ import java.util.concurrent.CompletionStage;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
-
-/**
- * Class that performs operations on the database regarding roles.
- */
+/** Class that performs operations on the database regarding roles. */
 public class RoleRepository {
-    private final DatabaseExecutionContext executionContext;
+  private final DatabaseExecutionContext executionContext;
 
-    /**
-     * Dependency injection
-     *
-     * @param ebeanConfig      the Ebean configuration to use
-     * @param executionContext The context to run completion stages on
-     */
-    @Inject
-    public RoleRepository(EbeanConfig ebeanConfig, DatabaseExecutionContext executionContext) {
-        this.executionContext = executionContext;
-    }
+  /**
+   * Dependency injection
+   *
+   * @param ebeanConfig the Ebean configuration to use
+   * @param executionContext The context to run completion stages on
+   */
+  @Inject
+  public RoleRepository(EbeanConfig ebeanConfig, DatabaseExecutionContext executionContext) {
+    this.executionContext = executionContext;
+  }
 
-    /**
-     * A function that gets the list of all the valid roles.
-     *
-     * @return the list of all the RoleType
-     */
-    public CompletionStage<List<Role>> getAllRoles() {
-        return supplyAsync(() -> Role.find.query().findList(), executionContext);
-    }
+  /**
+   * A function that gets the list of all the valid roles.
+   *
+   * @return the list of all the RoleType
+   */
+  public CompletionStage<List<Role>> getAllRoles() {
+    return supplyAsync(() -> Role.find.query().findList(), executionContext);
+  }
 
-    /**
-     * Function that gets the roles of a specific user from the database and returns them as a list in an async
-     * function.
-     *
-     * @param userId int the id of the user
-     * @return CompletionStage&ltList&ltRole&gt&gt
-     */
-    public CompletionStage<User> getUser(int userId) {
-        return supplyAsync(() -> User.find.byId(userId), executionContext);
-    }
+  /**
+   * Function that gets the roles of a specific user from the database and returns them as a list in
+   * an async function.
+   *
+   * @param userId int the id of the user
+   * @return CompletionStage&ltList&ltRole&gt&gt
+   */
+  public CompletionStage<User> getUser(int userId) {
+    return supplyAsync(() -> User.find.byId(userId), executionContext);
+  }
 
   /**
    * Gets a role from the database.
@@ -55,7 +52,7 @@ public class RoleRepository {
    * @param roleType the type of role.
    * @return the Role.
    */
-    public Role getRole(RoleType roleType) {
-        return Role.find.query().where().eq("role_type", roleType.name()).findOne();
-    }
+  Role getRole(RoleType roleType) {
+    return Role.find.query().where().eq("role_type", roleType.name()).findOne();
+  }
 }
